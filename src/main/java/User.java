0cf5b7a83b905185b8038f3f1d172d39a7283a42
zjk <94497950@qq.com>
2023-04-24 09:12:46 +0800

    add User.java

---
+++ src/main/java/User.java
@@ -0,0 +1,18 @@
+public class User {
+    String name;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
