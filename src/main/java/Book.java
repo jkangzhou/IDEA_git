54d18a17d9ba93fefb3b1c94326fc62a81b852db
zjk <94497950@qq.com>
2023-04-24 09:15:55 +0800

    add Book.java

---
+++ src/main/java/Book.java
@@ -0,0 +1,18 @@
+public class Book {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
